3985115065dac30e529f86f05a969b94660b45ae
Harshit shobhane <harshit.2022ug1047@iiitranchi.ac.in>
2025-09-06 02:28:48 +0530

    update

--- src/components/OurTopClinicNav.tsx
+++ src/components/OurTopClinicNav.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
-const logo = '/logo.svg';
+import logo from '../data/logo.svg'
 
 const CardNav = dynamic(() => import('./CardNav'), { ssr: false })
 
